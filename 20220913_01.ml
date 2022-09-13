eb37805cee051a3bc46cb5c6d8bcb45caa7c5172
doodoo1014 <49575899+doodoo1014@users.noreply.github.com>
2022-09-13 09:53:34 +0900

    Delete 20220913_01.ml

--- 20220913_01.ml
+++
@@ -1,7 +0,0 @@
-let square : int -> int
-= fun x -> x * x
-
-let sum_if_true : (int -> bool) -> int -> int -> int
-= fun test first second ->
-  (if test first then first else 0) + 
-  (if test second then second else 0);;
