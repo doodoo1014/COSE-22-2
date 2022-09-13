fb978d0267eb3d2c5dc179df577cb034fdaa6459
doodoo1014 <49575899+doodoo1014@users.noreply.github.com>
2022-09-13 10:29:53 +0900

    Update 20220913_03.ml

--- COSE212/20220913_03.ml
+++ COSE212/20220913_03.ml
@@ -2,18 +2,21 @@ let y = (1, "two", true);;
 
 let fst p = 
   match p with
-    | (fst, snd) -> fst
+    | (fst, _) -> fst
     (* # of elements of tuple p must be certain
-    | (fst, snd, thr) -> thr
+    | (fst, _, _) -> fst
       error: This pattern matches values of type 'a * 'b * 'c
-       but a pattern was expected which matches values of type 'd * 'e
+      but a pattern was expected which matches values of type 'd * 'e
     *)
     ;;
+
+let fst3 p =
+  match p with
+    | (a, _, _) -> a;;
 
 let snd p = match p with (_, snd) -> snd;;
 
 fst(1, 2);;
-fst(1, 2, 3);;
 snd(3, "true");;
 
 
@@ -30,3 +33,4 @@ mult2 (1, 2);;
 
 (* mult2 1 2;; *)
 (* error: because it mult2's input is tuple, but real input is two integers *)
+
